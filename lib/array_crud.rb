9668e14b7f6ca5ab4afd35b7552e242dd08cbf6f
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-10 02:24:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
 pets = ["cat", "dog", "fish", "rabbit"]
 pets << horse
+puts pets.inspect
 end
 
 def add_element_to_start_of_array(array, element)
